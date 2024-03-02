4f50b5f685a495e37662b1d9bccad8a1b522acba
TotallyNotAlvin <totallynotalvin@gmail.com>
2024-03-02 19:42:00 +0800

    fix: adding mason lsps

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,5 +1,5 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-	ensure_installed = {"tsserver", "lua_ls", "pyright"},
+	ensure_installed = { "tsserver", "lua_ls", "pyright", "elixir-ls", "ocaml-lsp", "gopls" },
 	automatic_installation = true,
 }
